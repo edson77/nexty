849c3b6f73af33359c5c7aa3e1601175fcabfe21
Bitjoka Edson <edsonelmar@gmail.com>
2022-07-26 20:04:46 +0100

    modification de la section

--- src/Routeur.js
+++ src/Routeur.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes,Route } from 'react-router-dom';
 import NavBar from "./components/header/navBar";
 import HomePage from "./views/home/homePage";
+import OwnerProfile from "./views/profile/owner/profile";
 
 const Routeur = () => {
     return (
@@ -10,6 +11,7 @@ const Routeur = () => {
             <main>
                 <Routes>
                     <Route exact path='/' element={<HomePage />}></Route>
+                    <Route exact path='/profile' element={<OwnerProfile />}></Route>
                 </Routes>
             </main>
         </>
